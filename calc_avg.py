f12fb28b396ef79a01ab3dba21c191a714ac9c6f
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2017-03-21 16:15:18 -0700

    Add duncan's avg finder

---
+++ calc_avg.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from datetime import datetime
+
+LENGTH = 47133
+
+DATA = pd.read_csv('data.csv')
+INCEDENTS = DATA.iterrows()
+
+def calc_avg_resp_time(incidents):
+    delta = 0
+    for incedent in incidents:
+        arrived = datetime.strptime(incedent[1]["Time Arrived on Scene"], '%m/%d/%Y %I:%M:%S %p')
+        callRecieved = datetime.strptime(incedent[1][0], '%m/%d/%Y %I:%M:%S %p')
+        deltaHour = (int(callRecieved.hour) - int(arrived.hour)) * 60
+        deltaMinute = abs(int(callRecieved.minute) - int(arrived.minute))
+        delta += deltaHour + deltaMinute
+
+    averageRespTime = delta / LENGTH
+
+    return averageRespTime
+
+print(calc_avg_resp_time(INCEDENTS))
