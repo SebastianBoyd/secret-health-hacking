af745cae50ce8106df048051f0b9d587a6f04c94
Max Starr <max.starr@techie.com>
2017-03-24 10:34:32 -0700

    Added res and vis components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { VisualizationsComponent } from './visualizations/visualizations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    ResourcesComponent,
+    VisualizationsComponent
   ],
   imports: [
     BrowserModule,
