82a4b331563324ef9d5a589eb4ac052ea8afe505
Duncan Grubbs <duncan.grubbs@gmail.com>
2017-03-24 10:08:45 -0700

    Make folders.

--- server/calc_avg.py
+++ server/calc_avg.py
@@ -9,11 +9,13 @@ INCEDENTS = DATA.iterrows()
 def calc_avg_resp_time(incidents):
     delta = 0
     for incedent in incidents:
+        print(incedent[1])
         arrived = datetime.strptime(incedent[1]["Time Arrived on Scene"], '%m/%d/%Y %I:%M:%S %p')
         callRecieved = datetime.strptime(incedent[1][0], '%m/%d/%Y %I:%M:%S %p')
         deltaHour = (int(callRecieved.hour) - int(arrived.hour)) * 60
         deltaMinute = abs(int(callRecieved.minute) - int(arrived.minute))
         delta += deltaHour + deltaMinute
+        break
 
     averageRespTime = delta / LENGTH
 
