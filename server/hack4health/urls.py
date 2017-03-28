92bb113062d2a230f2a5e9a8337bcdd4fe4f2b02
krashanoff <lkrashanoff2@gmail.com>
2017-03-27 20:26:12 -0700

    Resolve url issue

--- server/hack4health/urls.py
+++ server/hack4health/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^index/', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
