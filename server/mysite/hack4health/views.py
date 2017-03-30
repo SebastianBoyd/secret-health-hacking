b52091db7eb5b56798fb0e45fc20bd5e6cbdea44
Duncan Grubbs <duncan.grubbs@gmail.com>
2017-03-29 19:10:31 -0700

    More templating.

--- server/mysite/hack4health/views.py
+++ server/mysite/hack4health/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "home.html")
+    if (request.path == "/"):
+        return render(request, "home.html")
+    else:
+        path = request.path.replace('/', '')
+        template_name = path + ".html"
+        return render(request, template_name)
