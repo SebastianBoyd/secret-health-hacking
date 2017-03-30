f5b52d7284647936b38c5e51b1d6cdc2832d58da
Duncan Grubbs <duncan.grubbs@gmail.com>
2017-03-29 18:59:21 -0700

    Templates work.

--- server/mysite/hack4health/views.py
+++ server/mysite/hack4health/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Resp</h1>", "Content-Type: text/html")
+    return render(request, "home.html")
